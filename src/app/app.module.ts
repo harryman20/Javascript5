52b3a1e5af8b1b3e6a66da38a331e09a1bdd70c2
Harry Smith <Harrison.Smith62@stclairconnect.ca>
2019-02-28 15:55:44 -0500

    Something

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { DefaultTypePipe } from './default-type.pipe';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
